7c2dcfa28842972f02809a96a3ea0fc6250e8694
gk-sg <121740552+gk-sg@users.noreply.github.com>
2026-04-19 18:33:33 -0400

    Create removeFromArray function that can remove multiple inputs from a given array
    Function can take multiple inputs and remove them from the array.
    However, the function fails when there are repeated values in the array.
    For example: removeFromArray([1, 2, 2, 4], 2) will yield [1, 2, 4].

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-};
-
+function removeFromArray(array, ...args) {
+    for (arg of args) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === arg) {
+                array.splice(i, 1);
+            } else {
+                continue;
+            }
+        }
+    }
+    return array;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
