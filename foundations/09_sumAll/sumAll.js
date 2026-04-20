3c07095228e998a44fd607b07208c64843cd354c
gk-sg <121740552+gk-sg@users.noreply.github.com>
2026-04-19 21:56:50 -0400

    Create function that returns the sum of every integer between and including them

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if (int1 < 0 ||
+        int2 < 0 ||
+        !Number.isInteger(int1, int2) ||
+        typeof (int1, int2) != "number")
+        {
+        return "ERROR";
+    } else {
+        let range = [int1, int2].sort();
+        let sum = 0;
+        for (let i = range[0]; i <= range[1]; i++) {
+            sum += i;
+        } return sum;
+    }
 };
 
 // Do not edit below this line
