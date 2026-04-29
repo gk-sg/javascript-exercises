251ca90484c9bd5ba9ecce9b3db2d03d1005adcb
gk-sg <121740552+gk-sg@users.noreply.github.com>
2026-04-28 21:28:37 -0400

    Create palindromes function

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+let palindromes = function (word) {
+    let reversed = [...word.toLowerCase()].reverse().join('');
 
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz1234567890";
+
+    let cleaned = word
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('')
+
+    return cleaned === cleaned.split('').reverse().join('');
 };
 
 // Do not edit below this line
